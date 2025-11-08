7c009e6997bf49d00c76bb92c52d37875172aa0c
adeelatoz <adeelaslam.atozadvert@gmail.com>
2025-11-08 11:55:13 +0500

    fixed some issues

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,25 +32,49 @@ const app = express();
 //   credentials: true
 // }));
 
-const allowedOrigins = [
-  "https://atoz-mcrm-hj14v23s0-adeels-projects-8363061a.vercel.app",
-  "http://localhost:3000", // for local dev
-];
+process.on('uncaughtException', console.error);
+process.on('unhandledRejection', console.error);
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      console.log("❌ Blocked by CORS:", origin);
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+require('module-alias/register');
+const mongoose = require('mongoose');
+const { globSync } = require('glob');
+const path = require('path');
+
+// Make sure we are running node 7.6+
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+if (major < 20) {
+  console.log('Please upgrade your node.js version at least 20 or greater. 👌\n ');
+  process.exit();
+}
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env.local' });
+
+mongoose.connect(process.env.DATABASE);
+
+// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+mongoose.connection.on('error', (error) => {
+  console.log(
+    `1. 🔥 Common Error caused issue → : check your .env file first and add your mongodb url`
+  );
+  console.error(`2. 🚫 Error → : ${error.message}`);
+});
+
+const modelsFiles = globSync('./src/models/**/*.js');
+
+for (const filePath of modelsFiles) {
+  require(path.resolve(filePath));
+}
+
+// Start our app!
+const app = require('./app');
+app.set('port', process.env.PORT || 8888);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → On PORT : ${server.address().port}`);
+});
+
 
 
 app.use(cookieParser());
